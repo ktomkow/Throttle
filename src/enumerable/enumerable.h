0fb4c22504740c0dcd2f4e5538912ccc90dbea0f
ktomkow <krzysztof.tomkow@gmail.com>
2024-08-22 22:03:00 +0200

    WIP

--- src/enumerable/enumerable.h
+++ src/enumerable/enumerable.h
@@ -1,32 +1,20 @@
 #ifndef enumerable.h
-#define enumerable .h
+#define enumerable.h
 
 template<typename T>
-class Enumerator {
-public:
-  // virtual void moveNext();
-  // virtual T* getCurrent();
-};
-
-template<typename T>
-class Enumerable {
-public:
-  // virtual Enumerator<T> getEnumerator();
-};
-
-template<typename T>
-// class MyArray : public Enumerable<T> {
-class MyArray {
-
+class Collection {
 private:
   T* _collection;
   int _capacity;
   int _size;
   void extend();
+
 public:
-  MyArray();
-  void push(T*);
+  Collection();
+  void push(const T&);
+  // ~SimpleArray();
   // virtual Enumerator<T> getEnumerator() override;
 };
+
 
 #endif
