19d9dc8d280669ea388d871c3061d5895962198b
ktomkow <krzysztof.tomkow@gmail.com>
2024-09-03 23:46:41 +0200

    Simplify heartbeat

--- src/heartbeat/heartbeat.cpp
+++ src/heartbeat/heartbeat.cpp
@@ -3,23 +3,14 @@
 Heartbeat::Heartbeat() {
   Serial.println("Heartbeat initialization");
   _timestamp = millis();
-  _led = 10;
-  _state = false;
-  _isInitialRun = true;
-
-  pinMode(_led, OUTPUT);
-  digitalWrite(_led, _state == true ? LOW : HIGH);
 
   Serial.println("Heartbeat is initialized");
 }
 
 void Heartbeat::act() {
   unsigned long now = millis();
-  if (now - _timestamp > _hysteresisMillis || _isInitialRun == true) {
-    _isInitialRun = false;
+  if (now - _timestamp > _hysteresisMillis) {
     _timestamp = now;
-    digitalWrite(_led, _state == true ? LOW : HIGH);
-    _state = !_state;
     Serial.print("Heartbeat ");
     Serial.println(now, BIN);
   }
