e2be9e88ad3bf0fcfbd8e08c23b1a4b0991dc4f1
ktomkow <krzysztof.tomkow@gmail.com>
2024-09-04 00:00:51 +0200

    Comment out the old code that will be needed later

--- potentiometer.cpp
+++ potentiometer.cpp
@@ -1,74 +1,74 @@
-#include "potentiometer.h"
+// #include "potentiometer.h"
 
 
-ThrottlePotentiometer::ThrottlePotentiometer(int pin) {
-  _isInitialized = false;
-  _state = 0;
-  _pin = pin;
-  _minValue = 13;
-  _maxValue = 1011;
-  _isCalibrationInProgress = false;
-}
+// ThrottlePotentiometer::ThrottlePotentiometer(int pin) {
+//   _isInitialized = false;
+//   _state = 0;
+//   _pin = pin;
+//   _minValue = 13;
+//   _maxValue = 1011;
+//   _isCalibrationInProgress = false;
+// }
 
-int ThrottlePotentiometer::getPin() {
-  return _pin;
-}
+// int ThrottlePotentiometer::getPin() {
+//   return _pin;
+// }
 
-int ThrottlePotentiometer::getState() {
-  return _state;
-}
+// int ThrottlePotentiometer::getState() {
+//   return _state;
+// }
 
-void ThrottlePotentiometer::report() {
-}
+// void ThrottlePotentiometer::report() {
+// }
 
-void ThrottlePotentiometer::init() {
-  Serial.print("Initialized potentiometer on PIN: ");
-  Serial.println(_pin);
+// void ThrottlePotentiometer::init() {
+//   Serial.print("Initialized potentiometer on PIN: ");
+//   Serial.println(_pin);
 
-  _isInitialized = true;
-}
+//   _isInitialized = true;
+// }
 
-void ThrottlePotentiometer::act() {
-  if (_isInitialized == false) {
-    return;
-  }
+// void ThrottlePotentiometer::act() {
+//   if (_isInitialized == false) {
+//     return;
+//   }
 
-  if(_isCalibrationInProgress == true) {
-    calibrationAct();
-  }
+//   if(_isCalibrationInProgress == true) {
+//     calibrationAct();
+//   }
 
-  int currentState = analogRead(_pin);
-  if (currentState < _minValue) {
-    currentState = _minValue;
-  }
+//   int currentState = analogRead(_pin);
+//   if (currentState < _minValue) {
+//     currentState = _minValue;
+//   }
 
-  if (currentState > _maxValue) {
-    currentState = _maxValue;
-  }
+//   if (currentState > _maxValue) {
+//     currentState = _maxValue;
+//   }
 
-  _state = currentState;
-  Serial.println(_state);
-}
+//   _state = currentState;
+//   Serial.println(_state);
+// }
 
-void ThrottlePotentiometer::calibrationAct() {
-  if (_isCalibrationInProgress == false) {
-    return;
-  }
+// void ThrottlePotentiometer::calibrationAct() {
+//   if (_isCalibrationInProgress == false) {
+//     return;
+//   }
 
-}
+// }
 
-void ThrottlePotentiometer::startMinValueCalibration() {
-  if (_isCalibrationInProgress == true) {
-    return;
-  }
+// void ThrottlePotentiometer::startMinValueCalibration() {
+//   if (_isCalibrationInProgress == true) {
+//     return;
+//   }
 
-  _isCalibrationInProgress = true;
-}
+//   _isCalibrationInProgress = true;
+// }
 
-void ThrottlePotentiometer::startMaxValueCalibration() {
-  if (_isCalibrationInProgress == true) {
-    return;
-  }
+// void ThrottlePotentiometer::startMaxValueCalibration() {
+//   if (_isCalibrationInProgress == true) {
+//     return;
+//   }
 
-  _isCalibrationInProgress = true;
-}
+//   _isCalibrationInProgress = true;
+// }
