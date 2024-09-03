e737183c88cd2ca018339317c31e4903304b488d
ktomkow <krzysztof.tomkow@gmail.com>
2024-09-03 22:19:43 +0200

    Rework heartbeat

--- src/heartbeat.h
+++ src/heartbeat.h
@@ -3,36 +3,27 @@
 
 #include "Arduino.h"
 
-
-class Picker {
-public:
-  unsigned long take();
-};
-
-unsigned long Picker::take() {
-  return millis();
-}
-
 class Heartbeat {
 private:
   unsigned long _timestamp;
   int _led;
   bool _state;
   const unsigned int _hysteresisMillis = 1000;
-  Picker& _picker;
+
 public:
   void act();
-  Heartbeat(Picker&);
+  Heartbeat();
 };
 
 Heartbeat::Heartbeat(Picker& picker) {
+  Serial.println("Heartbeat initialization")
   _timestamp = millis();
   _picker = picker;
   _led = 10;
   _state = false;
 
-  pinMode(_led, OUTPUT);                            // pin as output.
-  digitalWrite(_led, _state == true ? LOW : HIGH);  // pin low
+  pinMode(_led, OUTPUT);
+  digitalWrite(_led, _state == true ? LOW : HIGH);
 
   Serial.println("Heartbeat is initialized");
 }
@@ -42,7 +33,7 @@ void Heartbeat::act() {
   unsigned long now = millis();
   if (now - _timestamp > _hysteresisMillis) {
     _timestamp = now;
-    digitalWrite(_led, _state == true ? LOW : HIGH);  // pin low
+    digitalWrite(_led, _state == true ? LOW : HIGH);
     _state = !_state;
     Serial.print("Heartbeat ");
     Serial.println(_picker.take(), BIN);
